cace5ee600cf69e014c8e016268eea8e8cfdcfd4
yogjin <youngjin988@gmail.com>
2020-12-27 07:28:04 +0000

    Use props

--- test/src/App.js
+++ test/src/App.js
@@ -1,4 +1,3 @@
-import logo from './logo.svg';
 import React, { Component } from 'react';
 import './App.css';
 
@@ -9,8 +8,8 @@ class Subject extends Component{//제목
 			//컴포넌트는 하나의 최상위 태그만 사용해야한다.
 			//->최상위태그 header
 			<header>
-				<h1>WEB</h1>
-				world wide web!
+				<h1>{this.props.title}</h1>
+				{this.props.sub}
 			</header>
 		);
 	}
@@ -19,11 +18,11 @@ class TOC extends Component{//목차
 	render(){
 		return(
 			<nav>		
-			<ul>
-				<li><a href="1.html">HTML</a></li>
-				<li><a href="2.html">CSS</a></li>
-				<li><a href="3.html">JavaScript</a></li>
-			</ul>
+				<ul>
+					<li><a href="1.html">HTML</a></li>
+					<li><a href="2.html">CSS</a></li>
+					<li><a href="3.html">JavaScript</a></li>
+				</ul>
 			</nav>
 		);
 	}
@@ -43,7 +42,8 @@ class App extends Component {
   return (
 	//->최상위태그 div
     <div className="App">
-    	<Subject></Subject>
+    	<Subject title="WEB" sub="world wide web!"></Subject>
+		  <Subject title="React" sub="for UI"></Subject>
 		<TOC></TOC>
 		<Content></Content>
     </div>
