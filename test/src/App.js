4c6f7c98a17f3a0f588a7b21b075b1e1a23c3668
yogjin <youngjin988@gmail.com>
2020-12-27 07:13:53 +0000

    add TOC(목차) Component

--- test/src/App.js
+++ test/src/App.js
@@ -3,23 +3,38 @@ import React, { Component } from 'react';
 import './App.css';
 
 //Component(class)명은 대문자로 시작.
-class Subject extends Component{
+class Subject extends Component{//제목
 	render(){//render()함수.
 		return (
 			//컴포넌트는 하나의 최상위 태그만 사용해야한다.
-			<header>//->최상위태그 header
+			//->최상위태그 header
+			<header>
 				<h1>WEB</h1>
 				world wide web!
 			</header>
 		);
 	}
 }
-
+class TOC extends Component{//목차
+	render(){
+		return(
+			<nav>		
+			<ul>
+				<li><a href="1.html">HTML</a></li>
+				<li><a href="2.html">CSS</a></li>
+				<li><a href="3.html">JavaScript</a></li>
+			</ul>
+			</nav>
+		);
+	}
+}
 class App extends Component {
  render(){
   return (
-    <div className="App">//->최상위태그 div
-      Hello, React!
+	//->최상위태그 div
+    <div className="App">
+    	<Subject></Subject>
+		<TOC></TOC>
     </div>
   );
  }
