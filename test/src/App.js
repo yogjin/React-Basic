9b4d38ab89dbbcabfbb14294ee6ed1cac1c69768
yogjin <youngjin988@gmail.com>
2020-12-28 14:56:34 +0000

    separate Subject,Content Components

--- test/src/App.js
+++ test/src/App.js
@@ -1,30 +1,9 @@
 import React, { Component } from 'react';
 import TOC from "./components/TOC";
+import Subject from "./components/Subject"
+import Content from "./components/Content"
 import './App.css';
 
-//Component(class)명은 대문자로 시작.
-class Subject extends Component{//제목
-	render(){//render()함수.
-		return (
-			//컴포넌트는 하나의 최상위 태그만 사용해야한다.
-			//->최상위태그 header
-			<header>
-				<h1>{this.props.title}</h1>
-				{this.props.sub}
-			</header>
-		);
-	}
-}
-class Content extends Component{
-	render(){
-		return(
-			<article>
-				<h2>{this.props.title}</h2>
-				{this.props.desc}
-			</article>
-		);
-	}
-}
 class App extends Component {
  render(){
   return (
