4effff0a53ed8a8878ee90d3cfb7432f08b1034d
yogjin <youngjin988@gmail.com>
2020-12-30 14:55:41 +0000

    using state : subject

--- test/src/App.js
+++ test/src/App.js
@@ -9,12 +9,15 @@ class App extends Component {
 	//그 Component를 초기화 시켜주고싶은 코드는 이 constructor안에 작성한다.
 	constructor(props){
 		super(props);
+		this.state = {
+			subject:{title:'WEB', sub:'World Wide Web!'}//객체생성
+		}
 	}
 	render(){
 		return (
 			//->최상위태그 div
     		<div className="App">
-    			<Subject title="WEB" sub="world wide web!"></Subject>
+    			<Subject title={this.state.subject.title} sub={this.state.subject.sub}></Subject>
 				<Subject title="React" sub="for UI"></Subject>
 				<TOC></TOC>
 				<Content title="props써보기" desc="짜잔"></Content>
