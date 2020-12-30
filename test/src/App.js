9c65b6fe1e0f79cd836eb3d6a0bc3e189b1e2e8e
yogjin <youngjin988@gmail.com>
2020-12-30 14:46:24 +0000

    코드정리

--- test/src/App.js
+++ test/src/App.js
@@ -5,17 +5,16 @@ import Content from "./components/Content"
 import './App.css';
 
 class App extends Component {
- render(){
-  return (
-	//->최상위태그 div
-    <div className="App">
-    	<Subject title="WEB" sub="world wide web!"></Subject>
-		  <Subject title="React" sub="for UI"></Subject>
-		<TOC></TOC>
-		<Content title="props써보기" desc="짜잔"></Content>
-    </div>
-  );
- }
+	render(){
+		return (
+			//->최상위태그 div
+    		<div className="App">
+    			<Subject title="WEB" sub="world wide web!"></Subject>
+				<Subject title="React" sub="for UI"></Subject>
+				<TOC></TOC>
+				<Content title="props써보기" desc="짜잔"></Content>
+    		</div>
+  		);
+ 	}
 }
-
 export default App;
