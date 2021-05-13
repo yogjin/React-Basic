258ef71c96630d81615e63c3565ecf44c04f4dca
yogjin <youngjin988@gmail.com>
2021-05-13 15:08:54 +0000

    3.4.2.3 한 Component에서 useState 여러번 사용하기

--- test/src/components/Say.js
+++ test/src/components/Say.js
@@ -5,11 +5,19 @@ const Say = () => {
   const onClickEnter = () => setText('어서오세요!');
   const onClickLeave = () => setText('안녕히가세요.');
   
+  const [color,setColor] = useState('black');
+  
   return (
     <div>
       <button onClick={onClickEnter}>입장</button>
       <button onClick={onClickLeave}>퇴장</button>
-      <h1>{text}</h1>
+      <h1 style={{color:color}}>{text}</h1>
+      <button style={{color:'red'}} onClick={()=>setColor('red')}>
+        빨강색
+      </button>
+      <button style={{color:'blue'}} onClick={()=>setColor('blue')}>
+        파란색
+      </button>
     </div>
   );
 };
