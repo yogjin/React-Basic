58c11e922855e481f6599b3441bd15990241ee0c
yogjin <youngjin988@gmail.com>
2021-05-13 14:39:19 +0000

    code formating

--- test/src/components/Counter.js
+++ test/src/components/Counter.js
@@ -1,34 +1,37 @@
-import React, {Component} from 'react';
+import React, { Component } from 'react';
 
-class Counter extends Component{
-	//state의 초깃값 설정하기
-	state = {
-		number : 0,
-		fixedNumber: 0
-	};
-	render(){
-		const {number,fixedNumber} = this.state;
-		return (
-			<div>
-				<h1>{number}</h1>
-				<h2>바뀌지 않는 값: {fixedNumber}</h2>
-				<button onClick={() => {
-						this.setState((prevState)=>{
-							return {
-								number : prevState.number+1
-							}
-						},
-						//callback함수로 this.setState가 끝난 후 특정 작업 실행하기.
-						()=>{
-							console.log('방금 setState가 호출되었다');
-							console.log(this.state);
-						});
-					}}
-				>
-				+1하기
-				</button>
-			</div>
-		);
-	}
+class Counter extends Component {
+  //state의 초깃값 설정하기
+  state = {
+    number: 0,
+    fixedNumber: 0,
+  };
+  render() {
+    const { number, fixedNumber } = this.state;
+    return (
+      <div>
+        <h1>{number}</h1>
+        <h2>바뀌지 않는 값: {fixedNumber}</h2>
+        <button
+          onClick={() => {
+            this.setState(
+              (prevState) => {
+                return {
+                  number: prevState.number + 1,
+                };
+              },
+              //callback함수로 this.setState가 끝난 후 특정 작업 실행하기.
+              () => {
+                console.log('방금 setState가 호출되었다');
+                console.log(this.state);
+              }
+            );
+          }}
+        >
+          +1하기
+        </button>
+      </div>
+    );
+  }
 }
 export default Counter;
