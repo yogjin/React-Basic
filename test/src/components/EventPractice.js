ca10feaada6cd7abce500eefa1c2bec6351cf4e3
yogjin <youngjin988@gmail.com>
2021-05-17 14:39:06 +0000

    4.2.2.1 onChange 이벤트

---
+++ test/src/components/EventPractice.js
@@ -0,0 +1,22 @@
+import React, { Component } from 'react';
+
+class EventPractice extends Component {
+	render() {
+    return(
+      <div>
+        <h1>이벤트연습</h1>
+        <input 
+          type="text"
+          name="message"
+          placeholder="아무거나 입력해보세요"
+          onChange={
+            (e)=>{
+              console.log(e.target.value);
+            }
+          }
+          ></input>
+      </div>
+    );
+  }
+}
+export default EventPractice;
